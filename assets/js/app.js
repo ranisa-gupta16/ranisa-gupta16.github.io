1b37e22a04f5ed7a266610feab4debb142e59584
ranisa-gupta16 <ranisa.tech@gmail.com>
2021-12-13 02:12:53 +1100

    Improvements

--- assets/js/app.js
+++ assets/js/app.js
@@ -133,7 +133,7 @@ let footer = $(`
                 <h6 class="display">Get in Touch</h6>
               </div>
                 <form>
-                  <form action="https://formcarry.com/s/S2thQbCqEvW" method="POST" accept-charset="UTF-8" >
+                  <form action="https://formcarry.com/s/IpdXeD4bY8o" method="POST" accept-charset="UTF-8" >
                   <input type="text" name="field1" placeholder="Your Name" required/>
                   <input type="email" name="field2" placeholder="Email Address"  required/>
                   <textarea name="field3" placeholder="Type your Message" required></textarea>
